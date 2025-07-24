5c373ee71aaff64c64e0a02673448c55e3da4cf8
PvonK <philippvonkesselstatt@gmail.com>
2025-07-23 21:24:56 -0300

    first commit

---
+++ makefile
@@ -0,0 +1,2 @@
+tonne: tonne.c
+	$(CC) tonne.c -o tonne -Wall -Wextra -pedantic -std=c99
---
+++ tonne.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+
+struct termios original_termios;
+
+void disableTermRawMode(){
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios);
+}
+
+void enableTermRawMode(){
+
+    // Check the value of the flags on the terminal and save them on the "original_termios" struct
+    tcgetattr(STDIN_FILENO, &original_termios);
+
+    // Register "disableTermRawMode" to always run on exit
+    atexit(disableTermRawMode);
+
+    // Define raw as a struct that holds flags
+    struct termios raw = original_termios;
+
+    // ECHO is a bitflag, defined as 00000000000000000000000000001000 in binary. We use the bitwise-NOT operator (~) on this value to get 11111111111111111111111111110111. We then bitwise-AND this value with the flags field, which forces the fourth bit in the flags field to become 0, and causes every other bit to retain its current value.
+    // We set the echo flag to 0 on "raw"
+    raw.c_lflag &= ~(ECHO);
+
+    // Sets the new flag to the terminal
+    // TCSAFLUSH argument specifies when to apply the change: in this case, it waits for all pending output to be written to the terminal, and also discards any input that hasn’t been read.
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
+}
+
+
+int main(){
+    enableTermRawMode();
+
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+
+    return 0;
+}
+
+
+// TODO
+
+// - [ ] Check what a struct is
+// - [ ] Check what & before the struct name is for
+// - [ ] Check why we put parenthesis on ECHO to do the bitwise NOT
